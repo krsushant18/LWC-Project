cc6375d476523670ba19ce7d595a1208c8c1c4e9
Sushant Kumar <sushku97@in.ibm.com>
2020-09-03 13:13:32 +0530

    test

--- force-app/main/default/lwc/helloLWC/helloLWC.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement, track } from 'lwc';
-export default class HelloLWC extends LightningElement {
-    @track greeting = 'SUSHANT';
-    changeHandler(event) {
-        this.greeting = event.target.value;
-    }
-}
